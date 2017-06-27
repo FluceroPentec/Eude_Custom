6c70b8f22a5e13e8ed5d6b261281c05baf355bb8
FluceroPentec <flucero@pentec.es>
2017-06-27 16:54:35 +0200

    integration v2

--- tests/utils_test.php
+++ tests/utils_test.php
@@ -1956,7 +1956,8 @@ class local_eudecustom_testcase extends advanced_testcase {
                 'CREATE;user8@testmail.com;Normal course 2;28-04-1970;1';
         $data5 = 'CREATE;user9@testmail.com;MI.COI.M01;29/04/1970;4' . PHP_EOL .
                 'CREATE;user10@testmail.com;MI.COI.M01;30/04/1970;5';
-        $data6 = 'CREATE;user1@testmail.com;MI.COI.M01;21/04/2017;4';
+        $data6 = 'CREATE;user1@testmail.com;MI.COI.M01;21/04/2017;4' . PHP_EOL .
+                'DELETE;user2@testmail.com;MI.COI.M01';
         /*
         $data6 = 'CREATE;user1@testmail.com;MI.COI.M01;21/04/2017;4' . PHP_EOL .
                 'DELETE;user2@testmail.com;MI.COI.M01'. PHP_EOL .
@@ -1970,7 +1971,6 @@ class local_eudecustom_testcase extends advanced_testcase {
         $expectedmatintrecords = $DB->get_records('local_eudecustom_mat_int');
         $expecteduserrecord1 = $DB->get_record('local_eudecustom_user', array('user_email' => $user1->email));
         $expecteduserrecord2 = $DB->get_record('local_eudecustom_user', array('user_email' => $user2->email));
-
         $this->assertTrue($result);
         $this->assertCount(2, $expectedmatintrecords);
         $this->assertEquals($user1->email, $expecteduserrecord1->user_email);
